fae80204269ba4d64ab2e668917386f1057909d1
franj1748 <71296318+franj1748@users.noreply.github.com>
2022-05-06 23:26:36 -0500

    Update juego.js

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -87,7 +87,7 @@
     const crearCarta = (carta, turno) => {
         
         const imgCarta = document.createElement('img');
-        imgCarta.src = `/assets/cartas/${carta}.png`;
+        imgCarta.src = `assets/cartas/${carta}.png`;
         imgCarta.classList.add('carta');
         cartasJugadores[turno].append(imgCarta);
 
